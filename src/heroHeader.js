f2920589d5fa6c81ece5c142d697ff160be53903
TDMK.Nathan <tdmk.nathan@Administrators-iMac.local>
2018-06-18 16:46:23 -0400

    Fix Bugs

--- src/heroHeader.js
+++ src/heroHeader.js
@@ -3,14 +3,14 @@ import LazyHero from 'react-lazy-hero';
 
 const IMAGES =
 [{
-        src: "https://c2.staticflickr.com/9/8817/28973449265_07e3aa5d2e_b.jpg",
+        url: "https://c2.staticflickr.com/9/8817/28973449265_07e3aa5d2e_b.jpg",
         thumbnail: "https://c2.staticflickr.com/9/8817/28973449265_07e3aa5d2e_n.jpg",
         thumbnailWidth: 320,
         thumbnailHeight: 174,
         caption: "After Rain (Jeshu John - designerspics.com)"
 },
 {
-        src: "https://c2.staticflickr.com/9/8356/28897120681_3b2c0f43e0_b.jpg",
+        url: "https://c2.staticflickr.com/9/8356/28897120681_3b2c0f43e0_b.jpg",
         thumbnail: "https://c2.staticflickr.com/9/8356/28897120681_3b2c0f43e0_n.jpg",
         thumbnailWidth: 320,
         thumbnailHeight: 212,
@@ -19,7 +19,7 @@ const IMAGES =
 },
 
 {
-        src: "https://images.unsplash.com/photo-1515764001020-c78a32a91265?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=e402378e4dd7653f978eb38ff32fb9e8&auto=format&fit=crop&w=948&q=80",
+        url: "https://images.unsplash.com/photo-1515764001020-c78a32a91265?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=e402378e4dd7653f978eb38ff32fb9e8&auto=format&fit=crop&w=948&q=80",
         thumbnail: "https://images.unsplash.com/photo-1515764001020-c78a32a91265?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=e402378e4dd7653f978eb38ff32fb9e8&auto=format&fit=crop&w=948&q=80",
         thumbnailWidth: 500,
         thumbnailHeight: 719,
@@ -27,21 +27,21 @@ const IMAGES =
 },
 
 {
-        src: "https://images.unsplash.com/photo-1507838175923-5689c9e3f527?ixlib=rb-0.3.5&s=4b7316b9854e1113842808a706e5f584&auto=format&fit=crop&w=1500&q=80",
+        url: "https://images.unsplash.com/photo-1507838175923-5689c9e3f527?ixlib=rb-0.3.5&s=4b7316b9854e1113842808a706e5f584&auto=format&fit=crop&w=1500&q=80",
         thumbnail: "https://images.unsplash.com/photo-1507838175923-5689c9e3f527?ixlib=rb-0.3.5&s=4b7316b9854e1113842808a706e5f584&auto=format&fit=crop&w=1500&q=80",
         thumbnailWidth: 7930,
         thumbnailHeight: 5289,
 },
 
 {
-        src: "https://images.unsplash.com/photo-1495900158145-fa1e1786861b?ixlib=rb-0.3.5&s=79070bda908b13b723e1663afa969da4&auto=format&fit=crop&w=1493&q=80",
+        url: "https://images.unsplash.com/photo-1495900158145-fa1e1786861b?ixlib=rb-0.3.5&s=79070bda908b13b723e1663afa969da4&auto=format&fit=crop&w=1493&q=80",
         thumbnail: "https://images.unsplash.com/photo-1495900158145-fa1e1786861b?ixlib=rb-0.3.5&s=79070bda908b13b723e1663afa969da4&auto=format&fit=crop&w=1493&q=80",
         thumbnailWidth: 5000,
         thumbnailHeight: 2800,
 },
 
 {
-        src: "https://c4.staticflickr.com/9/8887/28897124891_98c4fdd82b_b.jpg",
+        url: "https://c4.staticflickr.com/9/8887/28897124891_98c4fdd82b_b.jpg",
         thumbnail: "https://c4.staticflickr.com/9/8887/28897124891_98c4fdd82b_n.jpg",
         thumbnailWidth: 320,
         thumbnailHeight: 212
@@ -50,31 +50,29 @@ const IMAGES =
 
 
 class HeroHeader extends Component {
-  constructor(props) {
-    super(props);
-    this.changeHeroImage = this.changeHeroImage.bind(this);
-  }
   state = {images: [], title: "", imageSRC: "", count: 0}
 
-  componentWillReceiveProps(newProps) {
+  async componentWillReceiveProps(newProps) {
     console.log('New Props on the hero', newProps);
 
-    this.setState({ images: newProps.images, title: newProps.title });
+    await this.setState({ images: newProps.images, title: newProps.title });
+    console.log('did await', this.state);
     this.changeHeroImage();
   }
 
-  changeHeroImage(props) {
+  changeHeroImage = () => {
     // do whatever you like here
+    var images = IMAGES; //this.state.images;
     var count = this.state.count;
     var a = this;
     var url = "";
 
-    if (count >= IMAGES.length) {
+    if (count >= images.length) {
       count = 0;
     }
-    IMAGES.map(function(image, index) {
+    images.map(function(image, index) {
       if (index == count) {
-        url = image.src
+        url = image.url
       }
     })
     count++;
